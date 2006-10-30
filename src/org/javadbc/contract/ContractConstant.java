079fbe73353a1b8e517ee0db3f79924a132fff78
javier.hector <javier.hector@5bb3b6a0-cb1f-0410-8515-11d80430c4a1>
2006-10-30 09:48:34 +0000

    Jerarquia de clases que implementa las distintos features del lenguaje
    Constructor y Operation, ademas provee un algoritmo base para 
    recuperar las aserciones, resuevle el problema de la herencia.

---
+++ src/org/javadbc/contract/ContractConstant.java
@@ -0,0 +1,15 @@
+package org.javadbc.contract;
+
+public interface ContractConstant {
+
+	static final String interfacePattern = "interface";
+
+	static final String classPattern = "class";
+
+	static final String preKey = "pre";
+
+	static final String postKey = "post";
+
+	static final String invKey = "inv";
+
+}
